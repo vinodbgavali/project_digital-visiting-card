6d4f0a6aa2c4f67be7584546a9f58ddab06d1277
vinodbgavali <you@example.com>
2021-02-15 03:34:36 +0530

    First commit 1502-0330

---
+++ newJS.js
@@ -0,0 +1,22 @@
+toggleFlag = 0;
+const toggleMenu = () =>{
+    const menu = document.querySelector(".left_section_bottom");
+    const rightSection = document.querySelector(".right_section");
+
+
+    if(toggleFlag){
+       menu.style.left = "-150px";
+       rightSection.style.marginLeft = "10px";
+       rightSection.style.width = "97%";
+        console.log(rightSection);
+        toggleFlag = 0;
+    }
+    else{
+        menu.style.left= "10px";
+        rightSection.style.marginLeft = "140px";
+        rightSection.style.width = "80%";
+        console.log(rightSection);
+        toggleFlag = 1;
+    }
+
+}
